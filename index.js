9f155717ab2b9e27ff5a5300fd125d078c4cb779
beenice19 <byron217@yahoo.com>
2026-05-01 18:03:50 -0500

    Force open CORS for production testing

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const jwt = require("jsonwebtoken");
 const { Server } = require("socket.io");
 
 const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 const PORT = process.env.PORT || 8787;
 
 const corsOptions = {
